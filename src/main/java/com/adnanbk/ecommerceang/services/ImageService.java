c6d0a1b896e2d193b5e0098dd6ccb53ce4107d5c
Adnan <benkouide.adnane@gmail.com>
2020-08-20 02:37:06 +0300

    fifth commit

--- src/main/java/com/adnanbk/ecommerceang/services/ImageService.java
+++ src/main/java/com/adnanbk/ecommerceang/services/ImageService.java
@@ -1,42 +1,10 @@
 package com.adnanbk.ecommerceang.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.FileEditor;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Paths;
 
-@Service
-public class ImageService {
+public interface ImageService {
 
-
-    private ResourceLoader resourceLoader;
-
-   @Autowired
-    public ImageService(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
-
-
-
-    public String CreateImage(MultipartFile image) throws IOException {
-        String resoureString;
-
-        String fileName = image.getOriginalFilename().toLowerCase();
-
-            if (fileName != null && !fileName.isEmpty()) {
-                if(!fileName.endsWith(".jpg") && !fileName.endsWith(".png"))
-                    throw new IOException("Image type not supported , we accept only jpg or png files");
-                String dir = resourceLoader.getResource("classpath:/static").getFile().getAbsolutePath();
-                resoureString = dir + "/uploadingDir/" + fileName;
-                image.transferTo(Paths.get(resoureString));
-            }
-return fileName;
-
-    }
+   String CreateImage(MultipartFile multipartFile) throws IOException;
 }
--- src/main/java/com/adnanbk/ecommerceang/services/ProductService.java
+++ src/main/java/com/adnanbk/ecommerceang/services/ProductService.java
@@ -1,52 +1,13 @@
 package com.adnanbk.ecommerceang.services;
 
 import com.adnanbk.ecommerceang.models.Product;
-import com.adnanbk.ecommerceang.models.ProductCategory;
-import com.adnanbk.ecommerceang.reposetories.ProductCategoryRepository;
-import com.adnanbk.ecommerceang.reposetories.ProductRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Optional;
 
-@Service
-public class ProductService {
+public interface ProductService {
 
-    private ProductRepository productRepo;
-    private ProductCategoryRepository categoryRepo;
+    Optional<Product> updateProduct(Product product, String baseUrl);
 
-
-    public ProductService(ProductRepository productRepo, ProductCategoryRepository categoryRepo) {
-        this.productRepo = productRepo;
-        this.categoryRepo = categoryRepo;
-
-    }
-    @CacheEvict(value = {"byCategory","byId","byCategoryAndName","byName","allPro"},allEntries = true)
-    public Optional<Product> updateProduct(Product product,String baseUrl) {
-      var updatedProduct =  productRepo.findById(product.getId()).map(prod -> {
-
-          prod.setCategory(categoryRepo.findByCategoryName(product.getCategoryName()));
-              if(!product.getImageUrl().equals(prod.getImageUrl()))
-              prod.setImageUrl(baseUrl+"/uploadingDir/"+product.getImageUrl());
-          prod.setFromProduct(product);
-          productRepo.save(prod);
-          return prod;
-        });
-      return updatedProduct;
-    }
-    @CacheEvict(value = {"byCategory","byId","byCategoryAndName","byName","allPro"},allEntries = true)
-    public Product saveProduct(Product product,String baseUrl) {
-        if(!product.getCategoryName().isEmpty())
-        {
-            var cat = categoryRepo.findByCategoryName(product.getCategoryName());
-            product.setCategory(cat);
-        }
-        if(!product.getImageUrl().isEmpty())
-        product.setImageUrl(baseUrl+"/uploadingDir/"+product.getImageUrl());
-        return productRepo.save(product);
-    }
-
+    Product addProduct(Product product, String baseUrl);
 }
