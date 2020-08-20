b2d2f5f080b7a906455b474c21bd89e94f88a1bb
Adnan <benkouide.adnane@gmail.com>
2020-08-21 01:25:02 +0300

    commit

---
+++ src/main/java/com/adnanbk/ecommerceang/validations/ProductValidator.java
@@ -0,0 +1,41 @@
+package com.adnanbk.ecommerceang.validations;
+
+import com.adnanbk.ecommerceang.models.Product;
+import com.adnanbk.ecommerceang.reposetories.ProductRepository;
+import com.adnanbk.ecommerceang.services.ProductService;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class ProductValidator implements Validator {
+
+
+    private ProductRepository productRepository;
+
+    public ProductValidator(ProductRepository productRepository) {
+
+        this.productRepository = productRepository;
+    }
+
+
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Product.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Product product = (Product) o;
+
+       boolean isNameExist = productRepository.existsByIdAndNameIsNot(product.getId(),product.getName());
+       boolean isSkuExist =  productRepository.existsByIdAndSkuIsNot(product.getId(),product.getSku());
+
+       if(isNameExist)
+           errors.reject("Name already exists");
+
+        if(isSkuExist)
+            errors.reject( "Sku already exists");
+    }
+}
